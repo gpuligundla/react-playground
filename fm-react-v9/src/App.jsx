d0d84d093eb392feb58a1894fd8c96fb14020460
geethakrishna puligundla <gpuligu@clemson.edu>
2025-05-17 20:02:38 -0400

    added state management

--- fm-react-v9/src/App.jsx
+++ fm-react-v9/src/App.jsx
@@ -1,13 +1,12 @@
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = ()=>{
   return (
     <div>
       <h1 className="logo">Padre Geno's</h1>
-      <Pizza name="Pizza1" description="Pizza sample 1" image="/public/pizzas/pepperoni.webp" />
-      <Pizza name="Pizza2" description="Pizza sample 2" image="/public/pizzas/hawaiian.webp"/>
-      <Pizza name="Pizza3" description="Pizza sample 3"image="/public/pizzas/big_meat.webp"/>
+      <Order/>
     </div>
   )
 }
